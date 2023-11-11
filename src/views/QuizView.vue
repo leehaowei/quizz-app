33b4149de779b4c0f7424dcdc8f63da9710eb91d
Haowei Lee <haoweilee.tpe@gmail.com>
2023-11-11 22:32:53 +0800

    initial commit

---
+++ src/views/QuizView.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Question from "../components/Question.vue";
+import QuizHeader from "../components/QuizHeader.vue";
+import Result from "../components/Result.vue";
+import { useRoute } from "vue-router";
+import { ref, watch, computed } from "vue";
+import quizes from "../data/quizes.json"
+
+const route = useRoute()
+
+const quizId = parseInt(route.params.id)
+
+const quiz = quizes.find(q => q.id === quizId)
+
+const currentQuestionIndex = ref(0)
+const numberOfCorrectAnswers = ref(0)
+const showResults = ref(false)
+
+// step 1: define state. At this step there's no definition of changes
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+// step 2: define the action of changes, 
+// not necessary only changes or action related to questionStatus. It could do more
+// watch(() => currentQuestionIndex.value, () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+// })
+
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+const barPercentage = computed(() => `${currentQuestionIndex.value / quiz.questions.length * 100}%`)
+
+const onOptionSelected = (isCorrect) => {
+    if (isCorrect) {
+        numberOfCorrectAnswers.value++;
+    }
+
+
+    if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+        showResults.value = true;
+    }
+
+    currentQuestionIndex.value++
+}
+
+</script>
+
+<template>
+    <div>
+        <QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
+        <div>
+            <Question v-if="!showResults" :question="quiz.questions[currentQuestionIndex]"
+                @selectOption="onOptionSelected" />
+            <Result 
+                v-else 
+                :quizQuestionLength="quiz.questions.length"
+                :numberOfCorrectAnswers="numberOfCorrectAnswers"
+            />
+        </div>
+        <!-- <button @click="currentQuestionIndex++">Next Question</button> -->
+    </div>
+</template>
